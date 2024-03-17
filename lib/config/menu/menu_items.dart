57d61cd0349aba487ad23e6ef7a9aaa2fe1c5a7d
RchrdAriza <rchrdalv@gmail.com>
2024-03-16 19:20:01 -0500

    add progress indicator route, screen and menu option

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -23,5 +23,10 @@ const appMenuItems = <MenuItem>[
       title: 'tarjetas',
       subtitle: 'Un contenedor estilizado',
       link: "/cards",
-      icon: Icons.credit_card)
+      icon: Icons.credit_card),
+  MenuItem(
+      title: 'progressIndicator',
+      subtitle: 'Generales y controlados',
+      link: "/progress",
+      icon: Icons.refresh_rounded)
 ];
--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -6,6 +6,66 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress Indicator"),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text("Circular progress indicator"),
+          const SizedBox(height: 10),
+          const CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          ),
+          const SizedBox(height: 20),
+          const Text("Circular y lineal controlado"),
+          const SizedBox(height: 10),
+          _ControllerProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<double>(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100; // 0.0 1.0
+        }).takeWhile((value) => value <= 1),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                    value: progressValue,
+                    strokeWidth: 2,
+                    backgroundColor: Colors.black12),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: LinearProgressIndicator(
+                  value: progressValue,
+                ))
+              ],
+            ),
+          );
+        });
   }
 }
