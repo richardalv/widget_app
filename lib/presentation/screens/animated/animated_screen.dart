edef65df462b7d6eaa375da990d9defc3c35128b
RchrdAriza <rchrdalv@gmail.com>
2024-03-17 12:36:08 -0500

    animated screen completed

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,64 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
 }
 
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  /* final colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.yellow,
+    Colors.black,
+  ]; */
+
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(300) + 120;
+    borderRadius = random.nextInt(100) + 20;
+    color = Color.fromRGBO(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    // color = colors[random.nextInt(colors.length - 1)];
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeOutCubic,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius:
+                BorderRadius.circular(borderRadius <= 0 ? 0 : borderRadius),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
