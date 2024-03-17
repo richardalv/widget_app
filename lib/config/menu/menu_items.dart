3da8c82cdf73f920ef523043ad1c8da3aabe92a4
RchrdAriza <rchrdalv@gmail.com>
2024-03-16 19:56:39 -0500

    add snackbar menu option and route

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -25,8 +25,13 @@ const appMenuItems = <MenuItem>[
       link: "/cards",
       icon: Icons.credit_card),
   MenuItem(
-      title: 'progressIndicator',
+      title: 'progress indicator',
       subtitle: 'Generales y controlados',
       link: "/progress",
-      icon: Icons.refresh_rounded)
+      icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'snackbars y dialogos',
+      subtitle: 'Indicadores en pantalla',
+      link: "/snackbars",
+      icon: Icons.info_outline),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,6 +6,10 @@ class SnackbarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Snackbar y Dialogos"),
+      ),
+    );
   }
 }
